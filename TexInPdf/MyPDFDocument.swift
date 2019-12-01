81832f9efa461c65ea019a5a5adeb9b73c2062cb
chenxiaoyu233 <2016060106020@std.uestc.edu.cn>
2019-12-01 20:26:08 +0800

    [new feature] add a basic pdf viewer

---
+++ TexInPdf/MyPDFDocument.swift
@@ -0,0 +1,55 @@
+//
+//  Document.swift
+//  TexInPdf
+//
+//  Created by 陈小羽 on 2019/12/1.
+//  Copyright © 2019 chenxiaoyu233. All rights reserved.
+//
+
+import Cocoa
+import PDFKit
+import os.log
+
+class MyPDFDocument: NSDocument {
+    
+    var pdfDocument: PDFDocument = PDFDocument.init()
+    var pdfViewController: PDFViewController!
+    
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override class var autosavesInPlace: Bool {
+        return true
+    }
+
+    override func makeWindowControllers() {
+        // Returns the Storyboard that contains your Document window.
+        let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
+        let windowController = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier("PDF Window Controller")) as! NSWindowController
+        addWindowController(windowController)
+        if let VC = windowController.contentViewController as? PDFViewController {
+            pdfViewController = VC
+            pdfViewController.PDFViewer.document = pdfDocument
+        }
+    }
+
+    override func data(ofType typeName: String) throws -> Data {
+        return pdfDocument.dataRepresentation()!
+        // Insert code here to write your document to data of the specified type, throwing an error in case of failure.
+        // Alternatively, you could remove this method and override fileWrapper(ofType:), write(to:ofType:), or write(to:ofType:for:originalContentsURL:) instead.
+        //throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+    }
+
+    override func read(from data: Data, ofType typeName: String) throws {
+        pdfDocument = PDFDocument.init(data: data)!
+        // Insert code here to read your document from the given data of the specified type, throwing an error in case of failure.
+        // Alternatively, you could remove this method and override read(from:ofType:) instead.
+        // If you do, you should also override isEntireFileLoaded to return false if the contents are lazily loaded.
+        //throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+    }
+
+
+}
+
