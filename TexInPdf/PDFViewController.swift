554771d350848eeeb369a2470025dd9949d8c59d
chenxiaoyu233 <2016060106020@std.uestc.edu.cn>
2019-12-03 00:00:02 +0800

    [new feature] 使原先的标记添加功能支持「撤销」

--- TexInPdf/PDFViewController.swift
+++ TexInPdf/PDFViewController.swift
@@ -26,18 +26,51 @@ class PDFViewController: NSViewController {
     }
 
     @IBAction func AddNoteMenuItemSelected(_ sender: Any) {
-        if let curSel = PDFViewer.currentSelection {
-            os_log("you have selected something")
-        } else {
-            let alert = NSAlert()
-            alert.messageText = "Operation Fail"
-            alert.informativeText = "To use this function, you should select something"
-            alert.alertStyle = NSAlert.Style.warning
-            alert.addButton(withTitle: "OK")
-            alert.runModal()
-            return
-        }
+        addMarkAtSelectedPoint()
     }
 
+}
+
+/* MARK: - functions */
+
+extension PDFViewController {
+    func addMarkAtSelectedPoint() {
+        if let curSel = PDFViewer.currentSelection {
+            os_log("you have selected something")
+            var hlight: [PDFAnnotation] = [PDFAnnotation].init()
+            for page in curSel.pages {
+                let cur = PDFAnnotation.init(bounds: curSel.bounds(for: page),
+                                             forType: .square,
+                                             withProperties: nil)
+                hlight.append(cur)
+                page.addAnnotation(cur)
+            }
+            undoManager?.registerUndo(withTarget: self) { target in
+                target.removeMark(pages: curSel.pages, marks: hlight)
+            }
+        } else {
+            ShowAlertMessage(messageText: "Operation Fail",
+                             informativeText: "To use this function, you should select something")
+        }
+    }
+    func removeMark(pages: [PDFPage], marks: [PDFAnnotation]) {
+        for i in 0 ..< pages.count {
+            pages[i].removeAnnotation(marks[i])
+        }
+    }
+}
+
+
+/* MARK: - aux tools */
+extension PDFViewController {
+    func ShowAlertMessage(messageText: String, informativeText: String) {
+        let alert = NSAlert()
+        alert.messageText = messageText
+        alert.informativeText = informativeText
+        alert.alertStyle = NSAlert.Style.warning
+        alert.addButton(withTitle: "OK")
+        alert.runModal()
+        return
+    }
 }
 
