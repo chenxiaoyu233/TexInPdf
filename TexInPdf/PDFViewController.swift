2b3b7d355f0a180dac257a94cf372caafe96a546
chenxiaoyu233 <2016060106020@std.uestc.edu.cn>
2019-12-03 15:13:54 +0800

    [new feature] add a button to pdf that we could response it when it was clicked

--- TexInPdf/PDFViewController.swift
+++ TexInPdf/PDFViewController.swift
@@ -15,8 +15,8 @@ class PDFViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        addNoteButtonObserver()
     }
 
     override var representedObject: Any? {
@@ -24,24 +24,54 @@ class PDFViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+}
 
+extension PDFViewController{
+    
+    /// Action for menu item "Add Note", add a interactable note mark in the selected place
+    /// - Parameter sender: from menu item "Add Note"
     @IBAction func AddNoteMenuItemSelected(_ sender: Any) {
-        addMarkAtSelectedPoint()
+        addMarkAtSelectedPoint(forType: .widget) { mark in
+            mark.widgetFieldType = .button
+            mark.widgetControlType = .pushButtonControl
+            mark.backgroundColor = .init(calibratedRed: 1.0, green: 0.5, blue: 1.0, alpha: 0.2)
+            mark.color = .init(white: 1, alpha: 0)
+        }
     }
-
+    
+    /// Register the observer for widget button click in PDF
+    func addNoteButtonObserver() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(NoteButtonHitObserver(_:)),
+            name: .PDFViewAnnotationHit,
+            object: PDFViewer
+        )
+    }
+    
+    /// TODO: Open/Close the note content attached to the widget button in the PDF
+    /// - Parameter notification: Given by the NotificationCenter
+    @objc func NoteButtonHitObserver(_ notification: Notification) {
+        if let note: PDFAnnotation = notification.userInfo?["PDFAnnotationHit"] as! PDFAnnotation? {
+            os_log("button clicked")
+        }
+    }
 }
 
 /* MARK: - functions */
-
 extension PDFViewController {
-    func addMarkAtSelectedPoint() {
+    
+    /// Add a mark at the selected point in the pdf document
+    /// - Parameter forType: the annotation type you want
+    /// - Parameter modifier: customize your annotation in this closure
+    func addMarkAtSelectedPoint(forType: PDFAnnotationSubtype, modifier: (inout PDFAnnotation) -> Void = {annotation in}) {
         if let curSel = PDFViewer.currentSelection {
-            os_log("you have selected something")
             var hlight: [PDFAnnotation] = [PDFAnnotation].init()
             for page in curSel.pages {
-                let cur = PDFAnnotation.init(bounds: curSel.bounds(for: page),
-                                             forType: .square,
+                var cur = PDFAnnotation.init(bounds: curSel.bounds(for: page),
+                                             forType: forType,
                                              withProperties: nil)
+                modifier(&cur)
                 hlight.append(cur)
                 page.addAnnotation(cur)
             }
@@ -63,6 +93,10 @@ extension PDFViewController {
 
 /* MARK: - aux tools */
 extension PDFViewController {
+    
+    /// Show a short alert message to the user in a popup window
+    /// - Parameter messageText: the main reason of the alert
+    /// - Parameter informativeText: some hint sentences for user
     func ShowAlertMessage(messageText: String, informativeText: String) {
         let alert = NSAlert()
         alert.messageText = messageText
@@ -70,7 +104,6 @@ extension PDFViewController {
         alert.alertStyle = NSAlert.Style.warning
         alert.addButton(withTitle: "OK")
         alert.runModal()
-        return
     }
 }
 
