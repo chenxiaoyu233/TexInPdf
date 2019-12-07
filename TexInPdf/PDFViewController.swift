0f929a88d242987042a4e18d3b20d604673ac558
chenxiaoyu233 <2016060106020@std.uestc.edu.cn>
2019-12-07 11:28:20 +0800

    [new feature] 底部笔记栏宽度可调节

--- TexInPdf/PDFViewController.swift
+++ TexInPdf/PDFViewController.swift
@@ -18,7 +18,7 @@ class PDFViewController: NSViewController {
         // Do any additional setup after loading the view.
         addFetchPDFViewObserver()
         addPushPDFViewObserver()
-        PDFViewer.backgroundColor = NSColor.white
+        //PDFViewer.backgroundColor = NSColor.white
     }
 
     override var representedObject: Any? {
